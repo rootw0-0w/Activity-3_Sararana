de373affb7e0a9a40e9cbd5ce28b963575d84676
Alexa Sararana <alexa80804@gmail.com>
2026-09-25 11:29:48 +0800

    save

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] GameObject flameShoot;
+    [SerializeField] GameObject sniperShoot;
+    [SerializeField] GameObject shotgunShoot;
+    
+    public GameObject winPanel;
     void Start()
     {
         
@@ -20,5 +25,29 @@ public class Player : MonoBehaviour
 
     transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
     transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+
+    void FlameTurret()
+    {
+        
+    }
+
+    void ShotgunTurret()
+    {
+
+    }
+
+    void SniperTurret()
+    {
+        
+    }
+
+    void ActivateWinPanel()
+    {
+        if (winPanel != null)
+        {
+            winPanel.SetActive(true);
+
+        }
     }
 }
